8e2ff07273f54c134f887e41da5d1b18a019572e
Mashto <mashtogames@gmail.com>
2018-10-21 17:45:53 +0200

    Added opdracht Faculteit & Fibonacci

--- opdrachten/Opdracht2/Program.cs
+++ opdrachten/Opdracht2/Program.cs
@@ -6,11 +6,50 @@ namespace Opdracht2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
             
             //Faculteit
 
+            Console.WriteLine("Van welk nummer wil je de faculteit berekenen?");
+            int a = Convert.ToInt32(Console.ReadLine());
+            
+            int fac = a;
+
+            while (a > 2)
+            {
+                a--;
+                fac *= a;
+            }
+
+            Console.WriteLine("De faculteit van " + a + " is: " + fac);
+
+
             //Fibonacci
+
+            Console.WriteLine("Hoeveel elementen van Fibonacci wil je berekenen?");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int c = 0;
+            int fib = 1;
+
+            Console.WriteLine("-----");
+
+            if (b > 0)
+            {
+                Console.WriteLine("0");
+                if (b > 1)
+                {
+                    Console.WriteLine("1");
+                }
+            }
+
+            for (int i = 0; i < b - 2; i++)
+            {
+                int temp = c;
+                c = fib;
+                fib += temp;
+
+                Console.WriteLine(fib);
+            }
+        
         }
     }
 }
